f6f55cbfe6c81bbfc87bfd4fc8cebbb405d1dc3f
cty pr <gitcty@gmail.com>
2017-04-18 20:46:54 -0400

    Added second test and made it fail. first one passes

--- src/test/java/com/chetty/consulting/test/SvcTest.java
+++ src/test/java/com/chetty/consulting/test/SvcTest.java
@@ -4,11 +4,11 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class AppTest {
+public class SvcTest {
 
 	@Test
 	public void test() {
-		//fail("Not yet implemented");
+		fail("Not yet implemented");
 	}
 
 }
